a7f79224211c5fd174be32071716225252567671
Deep Kiran <cdeepkiran14@gmail.com>
2021-04-12 12:46:47 +0530

    Establish Firebase connection

---
+++ exchange-o-gram/src/firebase.js
@@ -0,0 +1,14 @@
+import firebase from "firebase";
+
+const firebaseApp=firebase.initializeApp({
+    apiKey: "AIzaSyAYxIx9V9_0yon-cQEhtXtxzj--P3ISqFs",
+    authDomain: "exchange-o-gram-f8d0e.firebaseapp.com",
+    projectId: "exchange-o-gram-f8d0e",
+    storageBucket: "exchange-o-gram-f8d0e.appspot.com",
+    messagingSenderId: "262200067676",
+    appId: "1:262200067676:web:980a08f9cbf0b98d948899",
+    measurementId: "G-Z5ETR429ZG"
+  })
+
+  const db=firebaseApp.firestore();
+  const projectStorage=firebaseApp.storage();
