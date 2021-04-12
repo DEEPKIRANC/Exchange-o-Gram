48f8d1d3ed006d3061b3ab95148cc9552eb21a62
Deep Kiran <cdeepkiran14@gmail.com>
2021-04-13 01:49:03 +0530

    Create new useFirestore custom hook

---
+++ exchange-o-gram/src/hooks/useFirestore.js
@@ -0,0 +1,19 @@
+import {useState,useEffect} from "react";
+import {db} from "../firebase";
+function useFirestore(collection) {
+    const [docs,setDocs]=useState([]);
+    
+    useEffect(() => {
+        const unsub=db.collection(collection).orderBy("likeCount","desc").onSnapshot((snap)=>{
+        let documents = [];
+        snap.forEach(doc=>{documents.push({...doc.data(),id:doc.id});    
+        }) ;
+        setDocs(documents);
+    });
+        return () => unsub();
+    }, [collection])
+    
+    return {docs}
+}
+
+export default useFirestore
